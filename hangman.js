6b07fa37df2fdf4a32c8a3a584c9fa76c938c8bb
Dave Cohen <dcohen18@gmail.com>
2017-06-30 21:25:43 -0500

    functions progress

--- hangman.js
+++ hangman.js
@@ -1,3 +1,4 @@
+const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
 
 function load_words(wordString) {
   return wordString.split(" ");
@@ -57,11 +58,60 @@ function get_guessed_word(secret_word, letters_guessed) {
     // letters have not yet been guessed.
 function get_available_letters(letters_guessed) {
   var not_guessed = '';
-  var lowerLetters = "abcdefghijklmnopqrstuvwxyz";
   for (let x = 0; x < lowerLetters.length; x++) {
     if (letters_guessed.indexOf(lowerLetters[x]) === -1) {
       not_guessed += lowerLetters[x];
     }
   }
   return not_guessed;
+}
+
+
+// ===MORE HELPER FUNCTIONS===
+function get_user_guess() {
+  /*
+  gets user input. checks if length == 1 and if in alphabet.
+  returns -1 if invalid. returns userGuess otherwise.
+  ADDITIONS: if userGuess == '*' then it's returned.
+  */
+  var userGuess = prompt('Please guess a letter: ').toLowerCase();
+  if (userGuess.length !== 1) return -1;
+  if (userGuess === '*') return userGuess;
+  if (lowerLetters.indexOf(userGuess) === -1) return -1;
+  return userGuess;  
+}
+
+function display_num_warnings(warnings) {
+  /*
+  Given a number of warnings, a properly formatted display is printed.
+  */
+  if (warnings > 1 || warnings == 0) {
+    console.log('You have ', warnings, ' warnings left: ');
+  } else if (warnings == 1) {
+    console.log('You have ', warnings, ' warning left: ');
+  } else {
+    console.log('You have no warnings left so you lose one guess: ')
+  }
+}
+
+function penalty(warnings) {
+  /*
+  Given a number of warnings, a penalty is assessed.
+  */
+  if (warnings < 0) return true;
+}
+
+//The total score is the number of  guesses_remaining  once the user has
+//guessed the  secret_word  times the number of unique letters in  secret_word .
+function num_unique_letters(secret_word) {
+  //Given a secret_word, the number of unique letters is returned.
+  var count = 0;
+  var unique = [];
+  for (var i = 0; i<secret_word.length; i++) {
+    if (unique.indexOf(secret_word[i]) === -1) {
+      unique.push(secret_word[i]);
+      count += 1;
+    }
+  }
+  return count;
 }
